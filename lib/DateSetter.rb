d82f11eb1dfe9d2ff3c538dadd4ba652625d0226
Szymon Rut <rut.szymon@gmail.com>
2012-11-07 16:56:27 +0100

    Added result, min_date_in_range and max_date_in_range methods to DateSetter class

--- lib/DateSetter.rb
+++ lib/DateSetter.rb
@@ -1,9 +1,9 @@
 class DateSetter
   Infinity = 1.0 / 0.0
-  attr_accessor :reference_date, :min_date, :max_date
+  attr_accessor :reference_date, :min_date, :max_date, :result, :range
 
   def valid?
-    return unless @reference_date || @min_date > @max_date
+    return unless @reference_date || @range || min_date.to_f > max_date.to_f
     (min_date.to_f..max_date.to_f).cover?(@reference_date.to_f)
   end
 
@@ -17,5 +17,25 @@ class DateSetter
 
   def max_date
     @max_date ||= Infinity
+  end
+
+  def result
+    @result = rand(min_date_in_range..max_date_in_range) if valid?
+  end
+
+  def min_date_in_range
+    if (reference_date - range).to_f < min_date.to_f
+      min_date
+    else
+      reference_date - range
+    end
+  end
+
+  def max_date_in_range
+    if (reference_date + range).to_f > max_date.to_f
+      max_date
+    else
+      reference_date + range
+    end
   end
 end
