8c2c77447bd3bde234da1dcb62387ee8fd52e2eb
Szymon Rut <rut.szymon@gmail.com>
2012-11-07 14:39:06 +0100

    Added valid?, invalid?, min_date and max_date methods to DataSetter class

--- lib/DateSetter.rb
+++ lib/DateSetter.rb
@@ -1,3 +1,21 @@
 class DateSetter
-  # Your code goes here...
+  Infinity = 1.0 / 0.0
+  attr_accessor :reference_date, :min_date, :max_date
+
+  def valid?
+    return unless @reference_date || @min_date > @max_date
+    (min_date.to_f..max_date.to_f).cover?(@reference_date.to_f)
+  end
+
+  def invalid?
+    !valid?
+  end
+
+  def min_date
+    @min_date ||= -Infinity
+  end
+
+  def max_date
+    @max_date ||= Infinity
+  end
 end
